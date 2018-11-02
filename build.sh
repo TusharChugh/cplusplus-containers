913b2e17d119198890d7289b866f05f0a83f45fc
Tushar Chugh <techie2sh@gmail.com>
2018-11-01 21:50:16 -0400

    modify for codecov

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ cmake -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. && make
 make test
 make string
 cd ..
-codecov -t 6cab29fe-1861-468f-903e-61e4fc96107e -X gcov
+codecov -t 029bebf0-142f-4f99-8449-ef1fd702274c -X gcov
