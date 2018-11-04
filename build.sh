63500bf0c04445597db0f08d6f2fb189433710ee
Tushar Chugh <techie2sh@gmail.com>
2018-11-04 15:14:51 -0500

    fix build script

--- build.sh
+++ build.sh
@@ -3,12 +3,7 @@
 echo "Environment: `uname -a`"
 echo "Compiler: `$CXX --version`"
 
-
-rm -r build
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=ON -DBUILD_TESTS=ON .. && make
-make test
-make string
-cd ..
-codecov -t 029bebf0-142f-4f99-8449-ef1fd702274c -X gcov
+./bin/unittest_container
